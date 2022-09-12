a37dd2cb18eebe1908d09f5f2691db6471437a59
12-miles <sundaydominic99@gmail.com>
2022-09-12 08:36:34 +0000

    Coding C is fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - serves as the main function or entrypoint
+ *main- serves as the main function or entrypoint
  *
- * return: this returns zero after executing all codes properly else returns a non zero output.
+ *Return- this returns zero after executing all codes properly else returns a non zero output.
  */
-
-int main(void)
+void main(void)
 {
-	puts("\ "programming is like building a multiluingual puzzzle"):
-		return (0):
+	puts("\"programming is like building a multiluingual puzzzle") ;
+	
+		Return(0) ;
 }
