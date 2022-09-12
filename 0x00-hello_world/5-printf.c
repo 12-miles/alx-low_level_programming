4de3d2ff26d35d25adbc8dfff4999ca2f2f0ee07
12-miles <sundaydominic99@gmail.com>
2022-09-12 11:26:06 +0000

    Coding C is fun

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* main - This is where the program starts
-*
-* Return: this will return (0) if the program is correct
-*/
+ *  * main - Entry point
+ *   *
+ *    * Return: always 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n ");
-		return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
