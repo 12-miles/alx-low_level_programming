9f4d68987933c1ba1c7c6d4c29542f680310214d
12-miles <sundaydominic99@gmail.com>
2022-09-14 22:28:08 +0000

    printing the alp

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
 {
-char alp = 'a';
-for (alp >= 'a' ; alp <= 'z'; ch++)
-{
-	putchar(alp);
-]
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
 _putchar('\n');
-return (0);
 }
